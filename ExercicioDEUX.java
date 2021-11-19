69b4dc22ced1cec7d78bfad6315fb234d0ec0167
Welington Melo <80336030+wmelodasilva@users.noreply.github.com>
2021-11-19 20:29:46 -0300

    Add files via upload

---
+++ ExercicioDEUX.java
@@ -0,0 +1,33 @@
+
+public class ExercicioDEUX {
+	
+	private int PaginasLidas;
+	private int Qntpagina;
+	private String Titulo;
+	
+  
+	public int getQntpagina() {
+		return Qntpagina;
+	}
+	public void setQntpagina(int qntpagina) {
+		Qntpagina = qntpagina;
+	}
+	public int getPaginasLidas() {
+		return PaginasLidas;
+	}
+	public int setPaginasLidas(int paginasLidas) {
+		return PaginasLidas = paginasLidas;
+	}
+	public String getTitulo() {
+		return Titulo;
+	}
+	public void setTitulo(String titulo) {
+		Titulo = titulo;
+	}
+	void progresso() {
+		System.out.print("O progresso do livro:"+this.Titulo +"  " + this.PaginasLidas * 100 / this.Qntpagina+ "%");
+	}
+	}
+	
+	
+
---
+++ Livro.java
@@ -0,0 +1,22 @@
+
+public class Livro {
+
+	public static void main(String[] args) {
+		
+		
+		
+		ExercicioDEUX livro = new ExercicioDEUX();
+		
+		livro.setTitulo("Bruna surfistinha");
+	
+		livro.setQntpagina(300);
+		
+		livro.setPaginasLidas(20);
+		
+		livro.progresso();
+		
+		
+				}
+	
+
+}
